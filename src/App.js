a34a96eb7c6b1c3ab2c4379745fb7b28e0619ad6
sarafat27 <sarafat2769@gmail.com>
2022-04-08 02:02:45 +0600

    user made

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 import app from './firebase.init';
-import { getAuth } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button';
+import { useState } from 'react';
 
 const auth = getAuth(app)
 function App() {
+  const [email, setEmail] = useState('')
+  const [pass, setPass] = useState('')
   const handleEmail = (event) => {
-    console.log(event.target.value)
+    setEmail(event.target.value)
   }
   const handlePass = event => {
-    console.log(event.target.value)
+    setPass(event.target.value)
   }
   const handleFormSubmit = event => {
-    console.log('submitted')
+    createUserWithEmailAndPassword(auth, email, pass)
+      .then(result => {
+        const user = result.user;
+        console.log(user)
+      })
+      .catch(error => {
+        console.error(error)
+      })
     event.preventDefault();
   }
   return (
